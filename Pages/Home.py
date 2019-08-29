3facb5e2afd5c999ca5bf2f448127f1a44e9e485
chitrangdakapoor <chitrangda.kapoor@gmail.com>
2019-08-29 17:29:53 +1000

    Delete Home.py

--- Pages/Home.py
+++
@@ -1,5 +0,0 @@
-class Home():
-    def __init__(self, driver):
-        self.driver = driver
-        self.logout_button_class_name = "logout"
-        self.women_category_xpath = '//*[@title="Women"]'
